99e436c08fad3a3c21767a8be2dc41111b33e6e1
Fernando Elotlan <zS20020852@estudiantes.uv.mx>
2022-10-26 21:47:32 -0500

    Agregar archivos de proyecto.

---
+++ BusinessLogic/Metodos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Metodos
+    {
+        public Metodos() { 
+
+        }
+        public bool Fernando()
+        {
+            return true;
+        }
+    }
+    
+}
---
+++ Host/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(WCF.Service1))) 
+            {
+                host.Open();
+                Console.WriteLine("Si jalo");
+                Console.ReadLine();
+            }
+        }
+    }
+}
