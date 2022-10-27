442700a7bd04e383764a01f62b8fd1d783cbc674
Fernando Elotlan <zS20020852@estudiantes.uv.mx>
2022-10-27 17:40:20 -0500

    Se agregaron funciionalidades a los DAO y se agregaron nuevos POCO

---
+++ BusinessLogic/DAO/JugadorDAO.cs
@@ -0,0 +1,109 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using BusinessLogic.POCO;
+
+namespace BusinessLogic.DAO
+{
+    internal class JugadorDAO
+    {
+        public static RespuestaLogin logear(String correo, String password)
+        {
+            RespuestaLogin respuestaBD = new RespuestaLogin();
+            MySqlConnection conexionBD = ConnectionUtil.obtenerConexion();
+
+            if (conexionBD != null)
+            {
+                try
+                {
+                    String consulta = "SELECT * FROM jugador WHERE email = @email AND password = @password";
+                    MySqlCommand mySqlCommand = new MySqlCommand(consulta, conexionBD);
+                    mySqlCommand.Parameters.AddWithValue("@email", correo);
+                    mySqlCommand.Parameters.AddWithValue("@password", password);
+                    MySqlDataReader respuesta = mySqlCommand.ExecuteReader();
+                    if (respuesta.Read())
+                    {
+                        respuestaBD.Error = false;
+                        respuestaBD.Mensaje = "Bienvenido al juego!!";
+
+                        Jugador jugadorBD = new Jugador();
+                        jugadorBD.Id = ((respuesta.IsDBNull(0)) ? 0 : respuesta.GetInt32(0));
+                        jugadorBD.Nombre = ((respuesta.IsDBNull(1)) ? "" : respuesta.GetString(1));
+                        jugadorBD.Email = ((respuesta.IsDBNull(6)) ? "" : respuesta.GetString(6));
+
+                        respuestaBD.DatosJugador = jugadorBD;
+                    }
+                    else
+                    {
+                        respuestaBD.Error = true;
+                        respuestaBD.Mensaje = "Contraseña o usuario inválidos";
+                        respuestaBD.DatosJugador = null;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    respuestaBD.Error = true;
+                    respuestaBD.Mensaje = "Hubo un problema de conexión, inténtelo más tarde";
+                    respuestaBD.DatosJugador = null;
+                }
+            }
+            else
+            {
+                respuestaBD.Error = true;
+                respuestaBD.Mensaje = "Hubo un problema de conexión, inténtelo más tarde";
+                respuestaBD.DatosJugador = null;
+            }
+
+            return respuestaBD;
+        }
+
+        public static Mensaje insertarJugador(Jugador jugadorRegistro)
+        {
+            Mensaje mensaje = new Mensaje();
+            MySqlConnection conexionBD = ConnectionUtil.obtenerConexion();
+            if (conexionBD != null)
+            {
+                try
+                {
+                    string sentencia = "INSERT INTO jugador (nombreCompleto, fechaNacimiento, password, email, puntajeGlobal) " +
+                                       "VALUES(@nombreCompleto,@fechaNacimiento,@telefono,@password,@email, @puntajeGlobal)";
+                    MySqlCommand mySqlCommand = new MySqlCommand(sentencia, conexionBD);
+                    mySqlCommand.Parameters.AddWithValue("@nombreCompleto", jugadorRegistro.Nombre);
+                    mySqlCommand.Parameters.AddWithValue("@fechaNacimiento", jugadorRegistro.FechaNacimiento);
+                    mySqlCommand.Parameters.AddWithValue("@password", jugadorRegistro.Password);
+                    mySqlCommand.Parameters.AddWithValue("@email", jugadorRegistro.Email);
+                    mySqlCommand.Parameters.AddWithValue("@puntajeGlobal", 0);
+                    mySqlCommand.Prepare();
+                    int filasAfectadas = mySqlCommand.ExecuteNonQuery();
+                    if (filasAfectadas > 0)
+                    {
+                        mensaje.Error = false;
+                        mensaje.MensajeRespuesta = "Jugador registrado con éxito";
+                    }
+                    else
+                    {
+                        mensaje.Error = true;
+                        mensaje.MensajeRespuesta = "Error al registrar el jugador";
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    mensaje.Error = true;
+                    mensaje.MensajeRespuesta = ex.Message;
+                }
+            }
+            else
+            {
+                mensaje.Error = true;
+                mensaje.MensajeRespuesta = "Por el momento no hay conexión con los servicios...";
+            }
+            return mensaje;
+        }
+    }
+}
---
+++ DataAccess/ConnectionUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace DataAccess
+{
+    public class ConnectionUtil
+    {
+        private static string SERVIDOR = "localhost";
+        private static string PUERTO = "3306";
+        private static string BASE_DATOS = "AhorcadoBD";
+        private static string USUARIO_BD = "AhorcadoGame";
+        private static string PASSWORD = "13F12E2M-";
+
+        public static MySqlConnection obtenerConexion()
+        {
+            MySqlConnection conexionBD = null;
+            try
+            {
+                string urlConexion = string.Format("server={0};" + "database={1};" +
+                                                   "username={2};" + "password={3};" + "port={4};",
+                                                   SERVIDOR, BASE_DATOS,
+                                                   USUARIO_BD, PASSWORD, PUERTO);
+                conexionBD = new MySqlConnection(urlConexion);
+                conexionBD.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return conexionBD;
+        }
+    }
+}
